07f7873abfd46ec313a672d0d25accaf0d5c9fe2
garyWork2 <garmstrong@integracolor.com>
2015-11-26 09:27:13 -0600

    Add Resource file for Db schema

--- HortImageRenamer.Core/ISettingsService.cs
+++ HortImageRenamer.Core/ISettingsService.cs
@@ -2,9 +2,24 @@
 {
   public interface ISettingsService
   {
+    /// <summary>
+    /// A concatenated string of file extensions that are not subject to renaming.
+    /// </summary>
     string LegalExtensions { get; }
 
+    /// <summary>
+    /// The extension to add when renaming.
+    /// </summary>
     string TargetExtension { get; }
+
+    /// <summary>
+    /// The root path of the directory that contains high resolution images.
+    /// </summary>
+    string ImageRoot { get; }
+
+    /// <summary>
+    /// The root path of the directory that contains thumbnail images.
+    /// </summary>
     string ThumbnailRoot { get; }
   }
 }
